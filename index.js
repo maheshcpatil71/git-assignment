8fe2b5d48bc8ad993b8fc6f5a290b89dac5918a5
maheshcpatil71 <maheshcpatil71@gmail.com>
2024-05-08 17:39:42 +0530

    added alert in JS File

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
     var password = document.getElementById("exampleInputPassword1").value;
     var isChecked = document.getElementById("exampleCheck1").checked;
 
+    
+    // var displayDiv = document.getElementById("displayData");
+    // displayDiv.innerHTML = "<h2>Form Data</h2>" +
+    //     "<p>Email: " + email + "</p>" +
+    //     "<p>Password: " + password + "</p>" +
+    //     "<p>Checkbox Checked: " + isChecked + "</p>";
 
-    var displayDiv = document.getElementById("displayData");
-    displayDiv.innerHTML = "<h2>Form Data</h2>" +
-        "<p>Email: " + email + "</p>" +
-        "<p>Password: " + password + "</p>" +
-        "<p>Checkbox Checked: " + isChecked + "</p>";
-
+    alert("Form data:\nEmail: " + email + "\nPassword: " + password + "\nCheckbox Checked: " + isChecked);
 
     console.log("Email:", email);
     console.log("Password:", password);
